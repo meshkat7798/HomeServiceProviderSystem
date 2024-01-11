c8367bbb944e2bff48a8f5bcf4fbc6ec9bbce8d3
Meshkat <meshkat.hashemian@yahoo.com>
2024-01-11 19:43:22 +0330

    base package added

---
+++ src/main/java/base/service/BaseEntityService.java
@@ -0,0 +1,27 @@
+package base.service;
+
+import base.entity.BaseEntity;
+
+import java.io.Serializable;
+import java.util.Collection;
+
+@SuppressWarnings("unused")
+public interface BaseEntityService<T extends BaseEntity<ID>, ID extends Serializable> {
+
+    void creatOrUpdate(T entity);
+
+    void delete(ID id);
+
+    T findById(ID id);
+
+    Collection<T> findAll();
+
+    boolean existsById(ID id);
+
+    void beginTransaction();
+
+    void commitTransaction();
+
+    void rollBack();
+
+}
